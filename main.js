686fd41ca9c660cf4106ffc8a2ceb9bbeb165ce5
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-05-30 02:57:51 +0430

    Creat display function for costimization of the books display after removing

--- main.js
+++ main.js
@@ -35,9 +35,9 @@ function displaybooks() {
     booklist.append(row);
     //Add event listener to remove button;
     const removebtn = document.querySelectorAll('.removeButton');
-    console.log('this is remove button',removebtn);
+    console.log('this is remove button', removebtn);
     removebtn.forEach(element => {
-        element.addEventListener('click',removeBook);
+        element.addEventListener('click', removeBook);
     });
 }
 
@@ -46,8 +46,22 @@ function removeBook(event) {
     console.log('runing removeBook');
     const indexToRemove = event.target.value;
     books.splice(indexToRemove, 1);
-    displaybooks();
+    display()
 }
+// display after removeing book
+function display() {
+    const booklist = document.querySelector('.booklist');
+    const row = document.createElement('tr');
+    booklist.innerHTML = '';
+    let index = 0;
+    books.forEach(element => {
+        const currentTitle = element.title;
+        const currentAuthor = element.author;
+        row.innerHTML = `${currentTitle}<br>${currentAuthor} <button class='removeButton' value="${index}">Remove</button> <hr>`;
+        index++;
+    });
+    booklist.append(row);
 
+}
 const addBookButton = document.querySelector('#addBook');
 addBookButton.addEventListener('click', addBooks);
