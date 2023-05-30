d81733ed08b2475af48964525de6dae21c01251f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-05-29 19:29:48 -0600

    fix lintern errors

--- main.js
+++ main.js
@@ -1,77 +1,75 @@
-//object constructor
+// object constructor
 function BooksConstructor(
-    title,
-    author
+  title,
+  author,
 ) {
-    this.title = title;
-    this.author = author;
+  this.title = title;
+  this.author = author;
 }
-//all books will be saved into the next array; 
+// all books will be saved into the next array;
 const books = [
 
-]
+];
 
-//function to add books;
+// function to add books;
 function addBooks() {
-    console.log('running addBooks');
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    books.push(new BooksConstructor(title, author));
-    display();
+  console.log('running addBooks');
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  books.push(new BooksConstructor(title, author));
+  display();
 }
-//displaying books
+// displaying books
 
 function display() {
-    console.log('runing display')
-    const tbody = document.querySelector('.booklist');
-    tbody.innerHTML = '';
-    console.log(tbody);
-    let index = 0;
-    books.forEach(element => {
-        const currentRow = document.createElement('tr');
-        const currentTitle = element.title;
-        const currentAuthor = element.author;
-        currentRow.innerHTML = `${currentTitle}<br>${currentAuthor} <button class='removeButton' value="${index}">Remove</button> <hr>`;
-        tbody.appendChild(currentRow);
-        index++
-    });
+  console.log('runing display');
+  const tbody = document.querySelector('.booklist');
+  tbody.innerHTML = '';
+  console.log(tbody);
+  let index = 0;
+  books.forEach((element) => {
+    const currentRow = document.createElement('tr');
+    const currentTitle = element.title;
+    const currentAuthor = element.author;
+    currentRow.innerHTML = `${currentTitle}<br>${currentAuthor} <button class='removeButton' value="${index}">Remove</button> <hr>`;
+    tbody.appendChild(currentRow);
+    index++;
+  });
 
-    //remove book from the books
-    function removeBook(event) {
-        console.log('runing removeBook');
-        const indexToRemove = event.target.value;
-        books.splice(indexToRemove, 1);
-        display()
-    }
-    // display after removeing book
+  // remove book from the books
+  function removeBook(event) {
+    console.log('runing removeBook');
+    const indexToRemove = event.target.value;
+    books.splice(indexToRemove, 1);
+    display();
+  }
+  // display after removeing book
 
-
-    const removebtn = document.querySelectorAll('.removeButton');
-    console.log('this is remove button', removebtn);
-    removebtn.forEach(element => {
-        element.addEventListener('click', removeBook);
-    });
+  const removebtn = document.querySelectorAll('.removeButton');
+  console.log('this is remove button', removebtn);
+  removebtn.forEach((element) => {
+    element.addEventListener('click', removeBook);
+  });
 }
 const addBookButton = document.querySelector('#addBook');
 addBookButton.addEventListener('click', addBooks);
 
-//presarving data to the local storage
+// presarving data to the local storage
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
-bookTitle.addEventListener('keydown', function() {
-    localStorage.setItem('title', bookTitle.value);
-})
-bookAuthor.addEventListener('keydown', function() {
-    localStorage.setItem('Author', bookAuthor.value);
-})
+bookTitle.addEventListener('keydown', () => {
+  localStorage.setItem('title', bookTitle.value);
+});
+bookAuthor.addEventListener('keydown', () => {
+  localStorage.setItem('Author', bookAuthor.value);
+});
 const browserTitle = localStorage.getItem('title');
 const browserAuthor = localStorage.getItem('Author');
 if (browserTitle) {
-    bookTitle.value = browserTitle;
-
+  bookTitle.value = browserTitle;
 }
 console.log(browserTitle);
 
 if (browserAuthor) {
-    bookAuthor.value = browserAuthor;
+  bookAuthor.value = browserAuthor;
 }
