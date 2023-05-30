6f16874c3cfa21a98a029571982294477d1ee14f
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-05-30 05:35:08 +0430

    Preseve data in the form with using local Storage

--- main.js
+++ main.js
@@ -25,32 +25,53 @@ function display() {
     console.log('runing display')
     const tbody = document.querySelector('.booklist');
     tbody.innerHTML = '';
-     console.log(tbody);
-     let index = 0;
-     books.forEach(element => {
-         const currentRow = document.createElement('tr');
-         const currentTitle = element.title;
-         const currentAuthor = element.author;
-         currentRow.innerHTML = `${currentTitle}<br>${currentAuthor} <button class='removeButton' value="${index}">Remove</button> <hr>`;
-         tbody.appendChild(currentRow);
-         index++
-     });
+    console.log(tbody);
+    let index = 0;
+    books.forEach(element => {
+        const currentRow = document.createElement('tr');
+        const currentTitle = element.title;
+        const currentAuthor = element.author;
+        currentRow.innerHTML = `${currentTitle}<br>${currentAuthor} <button class='removeButton' value="${index}">Remove</button> <hr>`;
+        tbody.appendChild(currentRow);
+        index++
+    });
 
-//remove book from the books
-function removeBook(event) {
-    console.log('runing removeBook');
-    const indexToRemove = event.target.value;
-    books.splice(indexToRemove, 1);
-    display()
-}
-// display after removeing book
+    //remove book from the books
+    function removeBook(event) {
+        console.log('runing removeBook');
+        const indexToRemove = event.target.value;
+        books.splice(indexToRemove, 1);
+        display()
+    }
+    // display after removeing book
 
 
-     const removebtn = document.querySelectorAll('.removeButton');
-     console.log('this is remove button', removebtn);
-     removebtn.forEach(element => {
-         element.addEventListener('click', removeBook);
-     });
+    const removebtn = document.querySelectorAll('.removeButton');
+    console.log('this is remove button', removebtn);
+    removebtn.forEach(element => {
+        element.addEventListener('click', removeBook);
+    });
 }
 const addBookButton = document.querySelector('#addBook');
 addBookButton.addEventListener('click', addBooks);
+
+//presarving data to the local storage
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+bookTitle.addEventListener('keydown', function() {
+    localStorage.setItem('title', bookTitle.value);
+})
+bookAuthor.addEventListener('keydown', function() {
+    localStorage.setItem('Author', bookAuthor.value);
+})
+const browserTitle = localStorage.getItem('title');
+const browserAuthor = localStorage.getItem('Author');
+if (browserTitle) {
+    bookTitle.value = browserTitle;
+
+}
+console.log(browserTitle);
+
+if (browserAuthor) {
+    bookAuthor.value = browserAuthor;
+}
