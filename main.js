fce1d4729c6db303e1fdeb7b40650aace7264af0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-05-29 19:31:39 -0600

    removes all console.log from main.js

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const books = [
 
 // function to add books;
 function addBooks() {
-  console.log('running addBooks');
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
   books.push(new BooksConstructor(title, author));
@@ -22,10 +21,8 @@ function addBooks() {
 // displaying books
 
 function display() {
-  console.log('runing display');
   const tbody = document.querySelector('.booklist');
   tbody.innerHTML = '';
-  console.log(tbody);
   let index = 0;
   books.forEach((element) => {
     const currentRow = document.createElement('tr');
@@ -38,7 +35,6 @@ function display() {
 
   // remove book from the books
   function removeBook(event) {
-    console.log('runing removeBook');
     const indexToRemove = event.target.value;
     books.splice(indexToRemove, 1);
     display();
@@ -46,7 +42,6 @@ function display() {
   // display after removeing book
 
   const removebtn = document.querySelectorAll('.removeButton');
-  console.log('this is remove button', removebtn);
   removebtn.forEach((element) => {
     element.addEventListener('click', removeBook);
   });
@@ -68,7 +63,6 @@ const browserAuthor = localStorage.getItem('Author');
 if (browserTitle) {
   bookTitle.value = browserTitle;
 }
-console.log(browserTitle);
 
 if (browserAuthor) {
   bookAuthor.value = browserAuthor;
