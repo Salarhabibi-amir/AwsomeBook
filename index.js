7e01b9e39c75c277f899b2c4bc542f8a383aa877
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-06-05 22:54:49 +0430

    Add the local time with using luxon library

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* --Navigation Bar--*/
 import hideOrShow from './modules/navbar.js';
 import Books from './modules/Books.js';
-import DateTime from './modules/luxon.js';
+import { DateTime } from './modules/luxon.js';
 
 /* --Navigation Bar--*/
 const navAnchors = document.querySelectorAll('.nav-anchors');
@@ -10,6 +10,11 @@ navAnchors.forEach((element) => {
 });
 /* --Navigation Bar--*/
 
+// Date and time
+const dateTime = document.querySelector('.date-time');
+const dt = DateTime.now();
+dateTime.innerHTML = `<span>Date: </span>${dt.year}\\ ${dt.month} \\ ${dt.day}&nbsp;&nbsp;&nbsp; <span>Time: </span> ${dt.hour}  :  ${dt.minute} : ${dt.second}`;
+// dateTime.innerHTML = dt;
 /* --Book Class--*/
 const booksCollection = new Books();
 booksCollection();
